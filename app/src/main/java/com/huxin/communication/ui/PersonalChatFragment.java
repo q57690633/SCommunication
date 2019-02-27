02f9fdce2ca59587c3bfd05b10f3d083bdaf935a
IceRainbow <3216071@163.com>
2019-02-27 19:52:48 +0800

    变更TUIKit集成方式为源码集成，聊天窗口加号按钮功能新增

--- app/src/main/java/com/huxin/communication/ui/PersonalChatFragment.java
+++ app/src/main/java/com/huxin/communication/ui/PersonalChatFragment.java
@@ -7,9 +7,21 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.huxin.communication.R;
+import com.huxin.communication.view.chatmenuunit.DataBaseUnit;
+import com.huxin.communication.view.chatmenuunit.FavoriteUnit;
+import com.huxin.communication.view.chatmenuunit.PhotoUnit;
+import com.huxin.communication.view.chatmenuunit.QiuGouUnit;
+import com.huxin.communication.view.chatmenuunit.QiuZuUnit;
+import com.huxin.communication.view.chatmenuunit.RentUnit;
+import com.huxin.communication.view.chatmenuunit.SellUnit;
+import com.sky.kylog.KyLog;
 import com.tencent.qcloud.uikit.business.chat.c2c.view.C2CChatPanel;
+import com.tencent.qcloud.uikit.business.chat.view.widget.MessageOperaUnit;
 import com.tencent.qcloud.uikit.common.BaseFragment;
 import com.tencent.qcloud.uikit.common.component.titlebar.PageTitleBar;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class PersonalChatFragment extends BaseFragment {
     private View mBaseView;
@@ -32,11 +44,13 @@ public class PersonalChatFragment extends BaseFragment {
 
 
     private void initView() {
+        List<MessageOperaUnit> units = initUnitList();
 
         //从布局文件中获取聊天面板组件
         chatPanel = mBaseView.findViewById(R.id.chat_panel);
         //单聊组件的默认UI和交互初始化
         chatPanel.initDefault();
+        chatPanel.initDefaultEvent();
         /*
          * 需要指定会话ID（即聊天对象的identify，具体可参考IMSDK接入文档）来加载聊天消息。在上一章节SessionClickListener中回调函数的参数SessionInfo对象中持有每一会话的会话ID，所以在会话列表点击时都可传入会话ID。
          * 特殊的如果用户应用不具备类似会话列表相关的组件，则需自行实现逻辑获取会话ID传入。
@@ -45,6 +59,7 @@ public class PersonalChatFragment extends BaseFragment {
             chatId = "Ljh2001";
         }
         chatPanel.setBaseChatId(chatId);
+        chatPanel.setMoreOperaUnits(units, false);
 
         //获取单聊面板的标题栏
         chatTitleBar = chatPanel.getTitleBar();
@@ -57,4 +72,24 @@ public class PersonalChatFragment extends BaseFragment {
         });
 
     }
+
+    private List<MessageOperaUnit> initUnitList() {
+        List<MessageOperaUnit> units = new ArrayList<>();
+        SellUnit sellUnit = new SellUnit();
+        RentUnit rentUnit = new RentUnit();
+        QiuGouUnit qiuGouUnit = new QiuGouUnit();
+        QiuZuUnit qiuZuUnit = new QiuZuUnit();
+        PhotoUnit photoUnit = new PhotoUnit();
+        DataBaseUnit dataBaseUnit = new DataBaseUnit();
+        FavoriteUnit favoriteUnit = new FavoriteUnit();
+        units.add(sellUnit);
+        units.add(rentUnit);
+        units.add(qiuGouUnit);
+        units.add(qiuZuUnit);
+        units.add(photoUnit);
+        units.add(dataBaseUnit);
+        units.add(favoriteUnit);
+        return units;
+    }
+
 }
---
+++ app/src/main/java/com/huxin/communication/view/chatmenuunit/RentUnit.java
@@ -0,0 +1,69 @@
+package com.huxin.communication.view.chatmenuunit;
+
+import android.view.View;
+
+import com.huxin.communication.R;
+import com.sky.kylog.KyLog;
+import com.tencent.qcloud.uikit.business.chat.view.widget.MessageOperaUnit;
+
+public class RentUnit extends MessageOperaUnit implements View.OnClickListener{
+    private int iconResId;
+
+    private int titleId;
+
+    private String action;
+
+    private View.OnClickListener onClickListener;
+
+    public RentUnit() {
+        iconResId = R.drawable.tab_icon_rent;
+        titleId = R.string.title_tab_icon_rent;
+        action = "This is action";
+        onClickListener = this;
+    }
+
+    @Override
+    public int getIconResId() {
+        return iconResId;
+    }
+
+    @Override
+    public void setIconResId(int iconResId) {
+        this.iconResId = iconResId;
+    }
+
+    @Override
+    public int getTitleId() {
+        return titleId;
+    }
+
+    @Override
+    public void setTitleId(int titleId) {
+        this.titleId = titleId;
+    }
+
+    @Override
+    public String getAction() {
+        return action;
+    }
+
+    @Override
+    public void setAction(String action) {
+        this.action = action;
+    }
+
+    @Override
+    public View.OnClickListener getOnClickListener() {
+        return onClickListener;
+    }
+
+    @Override
+    public void setOnClickListener(View.OnClickListener onClickListener) {
+        this.onClickListener = onClickListener;
+    }
+
+    @Override
+    public void onClick(View v) {
+        KyLog.i("This is a Log in RentUnit onClick");
+    }
+}
